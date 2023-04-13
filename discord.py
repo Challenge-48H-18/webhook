ad15f8a6e037d853ce8ace56d9cbe9bb8b4323ef
UwU <gaspard.cabecassegura@ynov.com>
2023-04-13 10:33:11 +0200

    webhook créé qui envoie un message de test

---
+++ discord.py
@@ -0,0 +1,4 @@
+from discord_webhook import DiscordWebhook
+
+webhook = DiscordWebhook(url='https://discord.com/api/webhooks/1095989356629594142/RF1aFmH18gYqNwELvmCIg5AHCTePAYj6CnV0T-cpwWgnQq2kOAfLvPqTnMxe5sEaSxC-', content='uwu')
+response = webhook.execute()
