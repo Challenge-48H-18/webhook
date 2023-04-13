7192adfeb192f970179548e37b77aeb22bed1e50
UwU <gaspard.cabecassegura@ynov.com>
2023-04-13 15:02:47 +0200

    call api fonctionnel

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+function edit() {
+
+  Getapi().then(data => {
+    console.log(data);
+    let id = data[0].id;
+    let stateus = data[0].status;
+    let post = data[0].posts;
+
+    let message = "ID: " + id + " | Status: " + stateus + " | Posts: " + post;
+    console.log(message);
+    sendMessage(message);
+  });
+}
+
 async function Getapi() {
   const response = await fetch('http://thegoodnetwork.fr/index.php/api/states', {
     method: "GET",
@@ -11,35 +25,15 @@ async function Getapi() {
   return data;
 }
 
-function edit() {
+function sendMessage(message) {
+  const request = new XMLHttpRequest();
+  request.open("POST", "https://discord.com/api/webhooks/1095989356629594142/RF1aFmH18gYqNwELvmCIg5AHCTePAYj6CnV0T-cpwWgnQq2kOAfLvPqTnMxe5sEaSxC-");
 
-  Getapi().then(data => {
-    // console.log(data);
-    let id = data[0].id;
-    let stateus = data[0].status;
-    let post = data[0].posts;
+  request.setRequestHeader('Content-type', 'application/json');
 
-    let message = "ID: " + id + " | Status: " + stateus + " | Posts: " + post;
-    console.log(message);
-  });
+  const params = {
+    content: message
+  }
+
+  request.send(JSON.stringify(params));
 }
-
-edit();
-
-
-// document.querySelector('.send').addEventListener('click', sendMessage);
-// function sendMessage() {
-
-//   const request = new XMLHttpRequest();
-//   request.open("POST", "https://discord.com/api/webhooks/1095989356629594142/RF1aFmH18gYqNwELvmCIg5AHCTePAYj6CnV0T-cpwWgnQq2kOAfLvPqTnMxe5sEaSxC-");
-
-//   // https://discord.com/api/webhooks/1095987174157066271/__SP4DFW3wGsCgBrfToclFgFvbjW4vXzNYvElAHyYnAVKaaxJGFCgek9k9EovHCiyUov
-//   // ttps://discord.com/api/webhooks/1095989356629594142/RF1aFmH18gYqNwELvmCIg5AHCTePAYj6CnV0T-cpwWgnQq2kOAfLvPqTnMxe5sEaSxC-
-//   request.setRequestHeader('Content-type', 'application/json');
-
-//   const params = {
-//     content: edit()
-//   }
-
-//   request.send(JSON.stringify(params));
-// }
--- test.js
+++
@@ -1,9 +0,0 @@
-function getapi(){
-    fetch('http://thegoodnetwork.fr/index.php/api/states')
-    .then((resp) => resp.json())
-    .then(function(data) {
-      console.log(data);
-    })
-  }
-  
-  getapi();
